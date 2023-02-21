d0e030cec20206998ca6985504e86b1bed26b0a7
ASIGBEY THEOPHILUS TETTEH <asigbey0242@gmail.com>
2023-02-21 04:53:47 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print _putchar with a new line
+ * Return: Always 0 (Success)
+ */
+void main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
