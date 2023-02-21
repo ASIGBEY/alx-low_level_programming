fabfe83dea1af512de0f45a0b33708f285b55810
ASIGBEY THEOPHILUS TETTEH <asigbey0242@gmail.com>
2023-02-21 02:55:07 -0800

    0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - The entry point of the program
+ * Description: A program that prints _putchar with a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
