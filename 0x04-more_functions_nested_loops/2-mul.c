8b633edbba0368355d4e392b64788c0c0247fe7d
ASIGBEY THEOPHILUS TETTEH <asigbey0242@gmail.com>
2023-02-23 14:28:23 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * mul - function that mutiplies two integers
+ * @a: first input
+ * @b: second input
+ * Return: multiplication result
+ */
+int mul(int a, int b)
+{return (a * b);
+}
