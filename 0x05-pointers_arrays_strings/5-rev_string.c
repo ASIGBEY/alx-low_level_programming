39cfd8377de201daebf2de425aeff9bf16e12f1e
ASIGBEY THEOPHILUS TETTEH <asigbey0242@gmail.com>
2023-02-27 14:28:27 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reversing a string
+ * @s: parameters
+ */
+void rev_string(char *s)
+{
+	char tmp;
+	int i, length1, lenght2;
+
+	length1 = 0;
+	lenght2 = 0;
+
+	while (s[length1] != '\0')
+		length1++;
+
+	length2 = length1 - 1;
+	for (i = 0; i < length1 / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[length2];
+		s[length2] = tmp;
+		length2 - = 1;
+	}
+	_putchar('\n');
+}
