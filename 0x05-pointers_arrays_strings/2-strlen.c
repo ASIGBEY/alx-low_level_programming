3a6f300be16b7510b61fd4e476f60b401ef1a66a
ASIGBEY THEOPHILUS TETTEH <asigbey0242@gmail.com>
2023-02-27 09:32:50 -0800

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - funtion to get the length of a string
+ * @s: sring pointer to passe to thsis funtion
+ * Return: returns length of the string
+ */
+int _strlen(char *s)
+	return (strlen(s));
+}
+
+
