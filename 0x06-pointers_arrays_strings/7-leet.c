9253f7c48ef6ba89be37197dc0494799cfa00f7c
ASIGBEY THEOPHILUS TETTEH <asigbey0242@gmail.com>
2023-03-02 10:06:04 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,7 +4,7 @@
  * @n: input value
  * Return: n value
  */
-char *leet(char *)
+char *leet(char *n)
 {
 	int i, j;
 	char s1[] = "aAeEoOtTlL";
@@ -12,13 +12,13 @@ char *leet(char *)
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (j = 0; j < 10; j++;
-				{
-				if (n[i] == s1[j])
-				{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
 				n[i] = s2[j];
-				}
-				}
-				}
-				return (0);
-				}
+			}
+		}
+	}
+	return (0);
+}
