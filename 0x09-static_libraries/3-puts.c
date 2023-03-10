866f8d4a793e86ec34b493d1ffd6285af2afdaa9
ASIGBEY THEOPHILUS TETTEH <asigbey0242@gmail.com>
2023-03-10 15:17:54 -0800

    fixed

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * _puts - function that puts s
  * @s: input
- * Return: _puts(s)
  */
 void _puts(char *s)
 {
-	_puts(s);
-	return _puts(s);
+	puts(s);
+}
+/**
+ * main - Entry point
+ * Return: 0 success
+ */
+int main(void)
+{
+/* Main code goes here */
+	return (0);
 }
