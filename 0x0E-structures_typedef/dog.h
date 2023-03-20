ac20447f5ef5b0bcf7f3bc07bb850f0a85a7a72a
ASIGBEY THEOPHILUS TETTEH <asigbey0242@gmail.com>
2023-03-20 08:45:34 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - dog's information
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
 
-struct dog{
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
