5e3ce15210999b05db6fc97b413c989d99f84384
ASIGBEY <asigbey0242@gmail.com>
2023-04-04 11:51:26 -0700

    #include lists.h
    /**
     * print_listint - prints all the elements of a linked list
     * @h: linked list of type listint_t to print
     *
     * Return: number of nodes
     */
    size_t print_listint(const listint_t *h)
    {
     size_t num = 0;
    
     while (h)
     {
     printf(%dn, h->n);
     num++;
     h = h->next;
     }
    
     return (num);
    }
    clear

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,4 @@
 #include "lists.h"
-<<<<<<< HEAD
-#include <stdlib.h>
-#include <stdio.h>
-=======
-#include <stdio.h>
-#include <stdlib.h>
->>>>>>> f6dcacf4e7b34a051b906b9d08779b77dbc0b8d2
-
 /**
 * print_listint - prints all the elements of a linked list
 * @h: linked list of type listint_t to print
@@ -15,7 +7,6 @@
 */
 size_t print_listint(const listint_t *h)
 {
-<<<<<<< HEAD
 	size_t number = 0;
 
 	while (h)
@@ -24,17 +15,5 @@ size_t print_listint(const listint_t *h)
 		number++;
 		h = h->next;
 	}
-	return (number);
-=======
-	 size_t number = 0;
-
-	  while (h)
-		   {
-			    printf("%d\n", h->n);
-			     number++;
-			      h = h->next;
-			       }
-
-	   return (number);
->>>>>>> f6dcacf4e7b34a051b906b9d08779b77dbc0b8d2
+	return (num);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,9 +1,23 @@
 #ifndef LISTS_H
 #define LISTS_H
-<<<<<<< HEAD
 
-=======
->>>>>>> f6dcacf4e7b34a051b906b9d08779b77dbc0b8d2
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* struct listint_s - singly linked list
+* @n: integer
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+* for the project
+*/
+typedef struct listint_s
+{
+ int n;
+ struct listint_s *next;
+} listint_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -15,4 +29,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
+
 #endif
