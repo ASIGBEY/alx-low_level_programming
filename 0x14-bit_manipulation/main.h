01dda5de32b36afcb6c1713eca365c32283c7f26
Asigbey Theophilus Tetteh <asigbey0242@gmail.com>
2023-08-31 05:53:33 -0700

    first

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+/**
+* enum work_shell - singly linked list
+* @n_neg: it holds negative -1
+* @n_pos: it holds positive 1
+* @emp: it holds zero 0
+* @bravo1: directs to perfect
+*/
+
+enum work_shell
+{
+	n_neg = -1,
+	n_pos = 1,
+	bravo1 = 0,
+	emp = 0
+};
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
