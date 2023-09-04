e66db71dab2eeba46219857cf03be59015d2870d
Asigbey Theophilus Tetteh <asigbey0242@gmail.com>
2023-09-04 10:59:52 -0700

    final

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -293,7 +293,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	{
 		free(header);
 		close_elf(o);
-		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[n_pos]);
 		exit(98);
 	}
 
